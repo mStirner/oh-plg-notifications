e6f33cb9033cc4d3dadfe9b2f01a1b6992612744
Marc Stirner <marc.stirner@hotmail.de>
2025-03-07 12:45:04 +0100

    init

--- README.md
+++ README.md
@@ -1,5 +1,56 @@
 # Introduction
-This is a boilerplate plugin.
+Allows to send notfication based on endpoint state value changes<br />
+
+# Configuration
+To enable this plugins, add labels on the endpoint you want to react upon:
+```
+notifications.enabled=true
+notifications.target[]={"state": <state id>, "threshold": <threshold value>, "operator": <operator>}
+```
+
+Example:
+```json
+{
+    "_id": "67c9658596013bcea8a778e0",
+    "name": "Particlesensor",
+    "device": "67c9658596013bcea8a778df",
+    "icon": "fa-solid fa-gauge-high",
+    "states": [
+        {
+            "name": "Particles (µg/m³)",
+            "alias": "particles",
+            "type": "number",
+            "min": 0,
+            "max": 25000,
+            "_id": "67c9658596013bcea8a778e1",
+            "description": null,
+            "value": 12,
+            "invert": false,
+            "timestamps": {
+                "created": 1741251973301,
+                "updated": 1741255942266
+            }
+        }
+    ],
+    "labels": [
+        ....
+        "notifications.enabled=true",
+        "notifications.target[]={\"state\":\"67c9658596013bcea8a778e1\", \"threshold\": 50, \"operator\": \"<=\"}"
+    ],
+    "timestamps": {
+        "created": 1741251973287,
+        "updated": 1741255942368
+    },
+    "enabled": true,
+    "room": null,
+    "commands": []
+}
+```
+
+Operator values can be `<`, `<=`, `>`, `>=`, `==`.<br />
+Where `a < b`, `a >= b` is `a` = state value, `b` threshold value.
+
+The example above, triggers a notifiction when the state values drop below or is equal to 50. `<value> <= <threshold>`, `12 <= 50`.
 
 # Installation
 1) Create a new plugin over the OpenHaus backend HTTP API
@@ -11,23 +62,16 @@ Add plugin item via HTTP API:<br />
 [PUT] `http://{{HOST}}:{{PORT}}/api/plugins/`
 ```json
 {
-   "name":"Plugin Boilerplate",
+   "name":"Notifications",
    "version": "1.0.0",
    "intents":[
-      "devices",
-      "endpoints",
-      "plugins",
-      "rooms",
-      "ssdp",
-      "store",
-      "users",
-      "vault"
+      "endpoints"
    ],
-   "uuid": "00000000-0000-0000-0000-000000000000"
+   "uuid": "a4cbb9bc-0cef-4fa2-902c-d4dfa3a3f7e7"
 }
-
 ```
+
 Mount the source code into the backend plugins folder
 ```sh
-sudo mount --bind ~/projects/OpenHaus/plugins/plugin-boilerplate/ ~/projects/OpenHaus/backend/plugins/00000000-0000-0000-0000-000000000000/
+sudo mount --bind ~/projects/OpenHaus/plugins/oh-plg-notifications/ ~/projects/OpenHaus/backend/plugins/a4cbb9bc-0cef-4fa2-902c-d4dfa3a3f7e7/
 ```
--- index.js
+++ index.js
@@ -1,41 +1,157 @@
+const { emitter, emitted } = require("../../system/component/class.events.js");
+const { deserialize } = require("../../system/component/class.labels.js");
+const { Notification } = require("../../system/notifications");
+
+
 module.exports = (info, logger, init) => {
     return init([
-        "devices",
-        "endpoints",
-        "plugins",
-        "rooms",
-        "ssdp",
-        "store",
-        "users",
-        "vault"
+        "endpoints"
     ], (scope, [
-        C_DEVICES,
-        C_ENDPOINTS,
-        C_PLUGINS,
-        C_ROOMS,
-        C_SSDP,
-        C_STORE,
-        C_USERS,
-        C_VAULT
+        C_ENDPOINTS
     ]) => {
 
 
-        // do something here with the components
-        // documentation about them can be found on:
-        // https://docs.open-haus.io/#/backend/components/
+        // holds wanted states & parameter for notification
+        // TODO: How to clean/remove state when endpoint is deleted?
+        let wanted = new Map();
 
 
-        // scope = plugin class instace
-        console.log(scope);
+        // handle events on component endpoints
+        // check if wanted states matches emitted changes
+        emitter.on(emitted, ({ args, event, component }) => {
+            if (event === "state" && args[0].type === "number" && component === "endpoints") {
+                wanted.forEach((params, state) => {
+
+                    // copied from scenes/trigger-types.js
+                    if (params.state === args[0]._id) {
+
+                        logger.verbose(`Change detected for state "${state.name}"`);
+
+                        let match = false;
+                        let a = state.value;
+                        let b = params.threshold;
+
+                        // a = state value
+                        // b = threshold
+                        switch (params.operator) {
+                            case "<":
+                                match = a < b;
+                                break;
+                            case ">":
+                                match = a > b;
+                                break;
+                            case "<=":
+                                match = a <= b;
+                                break;
+                            case ">=":
+                                match = a >= b;
+                                break;
+                            case "==":
+                                match = a == b;
+                                break;
+                            default:
+                                match = false;
+                        }
+
+                        // trigger & lock 
+                        if (match && !params.locked) {
+
+                            params.locked = true;
+
+                            logger.info(`Send notification for state`, state, params);
+
+                            let notification = new Notification({
+                                title: "State reached threshold value",
+                                message: `State "${state.name}" = ${state.value}. Threshold value (${params.operator}${params.threshold}) reached`,
+                                type: "info"
+                            });
+
+                            notification.publish();
+
+                        }
+
+                        // reset lock based on operator
+                        if (params.operator === "<" || params.operator === "<=") {
+                            if (a > b && params.locked) {
+
+                                params.locked = false;
+                                logger.debug(`Notfication lock reseted for state "${state.name}"`);
+
+                            }
+                        } else if (params.operator === ">" || params.operator === ">=") {
+                            if (a < b && params.locked) {
+
+                                params.locked = false;
+                                logger.debug(`Notfication lock reseted for state "${state.name}"`);
+
+                            }
+                        } else if (params.operator === "==") {
+                            if (a !== b && params.locked) {
+
+                                params.locked = false;
+                                logger.debug(`Notfication lock reseted for state "${state.name}"`);
+
+                            }
+                        }
+
+                    }
+
+                });
+            }
+        });
 
 
-        // example logger usage
-        logger.trace("Hello World from plugin ", info.name);
-        logger.verbose("Hello World from plugin ", info.name);
-        logger.debug("Hello World from plugin ", info.name);
-        logger.info("Hello World from plugin ", info.name);
-        logger.warn("Hello World from plugin ", info.name);
-        logger.error("Hello World from plugin ", info.name);
+        // warum so?
+        // scene definieren, die als trigger endpoint state hat
+        // und dann dort makro "notification" auausführen
+        // muss aber erst "dynmic makros" implementiert werden
+        // https://github.com/OpenHausIO/backend/issues/519
+        // oder warum nicht both?!
+        C_ENDPOINTS.found({
+            labels: [
+                "notifications.enabled=true",
+                "notifications.target[]=*"
+            ]
+        }, (endpoint) => {
+
+            let label = endpoint.labels.map((label) => {
+                return label.toString();
+            });
+
+            let { notifications } = deserialize(label);
+
+            logger.debug(`Notification object on endpoint "${endpoint.name}"`, notifications);
+
+            if (notifications?.target?.length <= 0) {
+                logger.warn(`No notifications targets found for endpoint "${endpoint.name}"`);
+                return;
+            }
+
+            notifications.target.map((target) => {
+
+                // parse json string from label
+                // target = {"state": "...", threashold: 25, operator: ">="}
+                return JSON.parse(target);
+
+            }).forEach((target) => {
+
+                let state = endpoint.states.find(({ _id }) => {
+                    return _id === target.state;
+                });
+
+                if (!state) {
+                    logger.warn(`Endpoint state "${target.state}" not found in endpoint "${endpoint.name}"`);
+                    return;
+                }
+
+                // patch threshold lock
+                target.locked = false;
+
+                wanted.set(state, target);
+
+            });
+
+        });
 
 
     });
